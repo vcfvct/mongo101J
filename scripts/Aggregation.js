544e5713f66bcda2bfd719f062384b14868cb2b1
leon <vcfvct@gmail.com>
2016-04-16 19:12:01 -0400

    a regex option for matching

--- scripts/Aggregation.js
+++ scripts/Aggregation.js
@@ -6,6 +6,7 @@ db.zips.aggregate(
         $group: { _id: "$fc", spop: {$sum: '$pop'}}
     },{
     	$match: {_id: {$in: ['0','1','2','3','4','5','6','7','8','9']}}
+    	// OR here we can use regex:  $match: {_id: {$regex: '[0-9]'}}
     },{
     	$group: {_id: null, aspop: {$sum: '$spop'}}
     }]

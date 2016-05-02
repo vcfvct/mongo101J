c5b157f81b493712872507b3d8070f915c3668fa
leon <vcfvct@gmail.com>
2016-05-02 08:50:41 -0400

    some format and comments

--- scripts/final.js
+++ scripts/final.js
@@ -1,25 +1,23 @@
-// Final 1
-db.messages.find({'headers.From': 'andrew.fastow@enron.com', 'headers.To': 'jeff.skilling@enron.com'}).count()
+//Final 1
+db.messages.find({ 'headers.From': 'andrew.fastow@enron.com', 'headers.To': 'jeff.skilling@enron.com' }).count()
 
 
-// Final 2
+//Final 2, use $addToSet to remove dups from the 'headers.To' list
 db.messages.aggregate([{
-	$project: {headers: '$headers'}
-},{
+    $project: { headers: '$headers' }
+}, {
     $unwind: '$headers.To'
 }, {
-	$group:{_id:'$_id', from: {$first: '$headers.From'}, to: {$addToSet: '$headers.To'}}
-},
-{
-	$unwind:'$to'
-},
-{
+    $group: { _id: '$_id', from: { $first: '$headers.From' }, to: { $addToSet: '$headers.To' } }
+}, {
+    $unwind: '$to'
+}, {
     $group: { _id: { from: '$from', to: '$to' }, sum: { $sum: 1 } }
 }, {
     $sort: { sum: -1 }
-},{
-	$limit:10
+}, {
+    $limit: 10
 }])
 
 
-// Final 3
+//Final 3
